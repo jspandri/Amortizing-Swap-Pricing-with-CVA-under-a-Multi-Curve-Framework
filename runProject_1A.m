9ed0be5543b9718ff22aa7528c08b00430540639
Jacopo Spandri <jspandri@gmail.com>
2026-05-19 09:08:41 +0200

    Add main .m

---
+++ runProject_1A.m
@@ -0,0 +1,18 @@
+% runProject_1A
+%
+% Project 1A: CVA Multi-Curve
+% Elisa Colombo, Stefano Marino, Jacopo Spandri
+% AY2025-2026
+%
+% to run:
+% > runProject_1A
+
+clear all;
+close all;
+clc;
+
+addpath("utilities\")
+addpath("data\")
+
+%% Settings
+formatData = 'dd/mm/yyyy';
