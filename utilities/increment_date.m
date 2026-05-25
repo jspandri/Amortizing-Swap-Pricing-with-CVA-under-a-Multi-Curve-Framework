c14538c8265122fdc0c51c73c6f51666988eb3b0
elisacolombo02 <elisacolombo02@gmail.com>
2026-05-25 13:21:00 +0200

    sistemata increment_date

--- utilities/increment_date.m
+++ utilities/increment_date.m
@@ -1,20 +1,27 @@
-function date = increment_date(t0, d, m, y)
-% Increment (or even decrease) initial date by a number of given days,
-% months and years.
+function date_num = increment_date(t0, d, m, y)
+% INCREMENT_DATE Increments or decrements a date by a specified 
+% number of days, months, and years.
 %
 % INPUTS:
-%   t0  - initial date
-%   d   - days to add (or subtract)
-%   m   - months to add (or subtract)
-%   y   - years to add (or subtract)
+%   t0  - Initial date (can be a datenum or a datetime object)
+%   d   - Number of days to add (or subtract)
+%   m   - Number of months to add (or subtract)
+%   y   - Number of years to add (or subtract)
 %
 % OUTPUTS:
-%   date    - final date
+%   date_num - The final date expressed as a datenum (numeric serial date)
 
-% Extract calendar date
-[Y,M,D] = datevec(t0);
+    % Convert t0 to a datetime object
+    if isnumeric(t0)
+        dt = datetime(t0, 'ConvertFrom', 'datenum');
+    else
+        dt = t0;
+    end
 
-% Add day, months, years
-date = datenum(Y+y, M+m, D+d);
+    % Add day, months, years according to the calendar
+    dt = dt + calyears(y) + calmonths(m);
+    dt = dt + days(d);
 
+    % Convert back to datenum
+    date_num = datenum(dt);
 end
