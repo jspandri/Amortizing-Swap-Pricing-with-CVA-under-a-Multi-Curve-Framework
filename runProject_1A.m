52583a21bda1318567c1ab37fb63e21d7879ec5a
Jacopo Spandri <jspandri@gmail.com>
2026-05-28 10:51:05 +0200

    cleaning main script

--- runProject_1A.m
+++ runProject_1A.m
@@ -17,28 +17,38 @@ addpath("ex3\")
 addpath("ex2\")
 addpath("ex_4\")
 
-%% Settings
-
-formatData = 'dd/mm/yyyy';
-settlement = datenum("28-Jun-2022");
-
 %% Read data
 
-[euriborSet, estrSet] = read_bootstrap_data("20220626_Curve.xlsx", settlement);
+% 2022 Data ---------------------------------------------------------------
+trade_date_22 = datenum("24-Jun-2022");
+settlement_22 = datewrkdy(trade_date_22, 3); % Find settlement date (+2BD, 
+                                             % datewrkdy needs offset+1=3)
 
-scheduleSwap = read_amortizing_plan('SwapAmortizingPlan_v1', 'SwapPlan');
+% Bootstrap data
+[euriborSet_22, estrSet_22] = read_bootstrap_data("20220626_Curve.xlsx", settlement_22);
+% Volatility matrix data
+vol_data_22 = read_vol_matrix_data("20220626_vol_matrix.xlsx");
+% Swap amortizing plan
+scheduleSwap_22 = read_amortizing_plan('SwapAmortizingPlan_v1', 'SwapPlan');
 
-vol_data = read_vol_matrix_data("20220626_vol_matrix.xlsx", settlement);
 
+% 2023 Data ---------------------------------------------------------------
+trade_date_23 = datenum("31-Jan-2023");
+settlement_23 = datewrkdy(trade_date_23, 3);
+
+% Bootstrap data
+[euriborSet_23, estrSet_23] = read_bootstrap_data("20230131_Curve.xlsx", settlement_23);
+% Volatility matrix data
+vol_data_23 = read_vol_matrix_data("20230131_vol_matrix.xlsx");
 
 %% 1) Multi-Curve Bootstrap
 
-[discountCurve, pseudoCurve] = multi_curve_bootstrap(euriborSet, estrSet);
+[discountCurve_22, pseudoCurve_22] = multi_curve_bootstrap(euriborSet_22, estrSet_22);
 
 %% Point 2-- Risk Free Amortizing Swap Pricing--
 K_strike    = 0.0221; 
-swapMarketData = precompute_swap_market_data(settlement, scheduleSwap, discountCurve, pseudoCurve);
-[NPV_riskfree, PV_fixed, PV_float] = swap_riskfree_npv(scheduleSwap, swapMarketData, K_strike);
+swapMarketData = precompute_swap_market_data(settlement_22, scheduleSwap_22, discountCurve_22, pseudoCurve_22);
+[NPV_riskfree, PV_fixed, PV_float] = swap_riskfree_npv(scheduleSwap_22, swapMarketData, K_strike);
 
 %% Point 3--Amortizing Swap Pricing with CVA: simplified approach--
 
@@ -46,51 +56,45 @@ RecoveryRate               = 0.4;
 CDS_spreads                = [300; 500] * 1e-4; % 300 bps and 500 bps
 HazardRates                = CDS_spreads / (1 - RecoveryRate);
 NPV = zeros(1,2);
-EE_profile = zeros(length(scheduleSwap.payDates),2) ;
+EE_profile = zeros(length(scheduleSwap_22.payDates),2) ;
 Total_CVA = zeros(1,2);
 
 for i= 1:2
     [Total_CVA(i), EE_profile(:, i)] = calculate_cva_bachelier(...
-        settlement, scheduleSwap, swapMarketData, vol_matrix, HazardRates(i), RecoveryRate,K_strike,discountCurve);
+        settlement_22, scheduleSwap_22, swapMarketData, vol_data_22, HazardRates(i), RecoveryRate,K_strike,discountCurve_22);
     NPV(i) = NPV_riskfree-Total_CVA(i);
 end
-plot_expected_exposures(scheduleSwap.payDates, EE_profile(:,1), EE_profile(:,2), CDS_spreads);
+plot_expected_exposures(scheduleSwap_22.payDates, EE_profile(:,1), EE_profile(:,2), CDS_spreads);
 
 %% Point 4 -- Unwinding
 
-trade_date = datenum("31-Jan-2023");
-settlement31 = following_day_convention(trade_date, 2, 0, 0, 1, false);
 maturity_date_not_adjusted = datenum("28-Jun-2037");
-notional_amortized = scheduleSwap.notionals; 
-
-% Read the Bachelier volatility matrix at the unwinding date
-vol_matrix31 = read_vol_matrix_data("20230131_vol_matrix.xlsx", settlement31);
+notional_amortized = scheduleSwap_22.notionals; 
 
 % Define the known historical fixing rate for the ongoing period (2.202%)
 past_fixing_rate = 0.02202;
 
-% Load mkt data and bootstrap the OIS discount curve and Euribor pseudo-discount curve at the unwinding date
-[euriborSet31, estrSet31] = read_bootstrap_data("20230131_Curve.xlsx", settlement31);
-[discountCurve31, pseudoCurve31] = multi_curve_bootstrap(euriborSet31, estrSet31); 
+% Bootstrap the OIS discount curve and Euribor pseudo-discount curve at the unwinding date
+[discountCurve_23, pseudoCurve_23] = multi_curve_bootstrap(euriborSet_23, estrSet_23); 
 
 % Generate the swap schedule starting from the new settlement date
-scheduleSwap31 = generate_swap_schedule(settlement31, settlement, ...
+scheduleSwap_23 = generate_swap_schedule(settlement_23, settlement_22, ...
     maturity_date_not_adjusted, notional_amortized);
 
 % Compute the Risk-Free Net Present Value (NPV) of the swap at the unwinding date
-NPV_RF = swap_riskfree_npv_v2(settlement31, scheduleSwap31, K_strike, ...
-    discountCurve31, pseudoCurve31, past_fixing_rate);
+NPV_RF = swap_riskfree_npv_v2(settlement_23, scheduleSwap_23, K_strike, ...
+    discountCurve_23, pseudoCurve_23, past_fixing_rate);
 
 % Compute CVA and Expected Exposure profile for the 300 bps CDS spread at unwinding
-[CVA_300, EE_300] = calculate_cva_bachelier_v2(settlement31, scheduleSwap31, K_strike, ...
-    discountCurve31, pseudoCurve31, vol_matrix31, HazardRates(1), RecoveryRate, past_fixing_rate);
+[CVA_300, EE_300] = calculate_cva_bachelier_v2(settlement_23, scheduleSwap_23, K_strike, ...
+    discountCurve_23, pseudoCurve_23, vol_data_23, HazardRates(1), RecoveryRate, past_fixing_rate);
 
 % Compute CVA and Expected Exposure profile for the 500 bps CDS spread at unwinding
-[CVA_500, EE_500] = calculate_cva_bachelier_v2(settlement31, scheduleSwap31, K_strike, ...
-    discountCurve31, pseudoCurve31, vol_matrix31, HazardRates(2), RecoveryRate, past_fixing_rate);
+[CVA_500, EE_500] = calculate_cva_bachelier_v2(settlement_23, scheduleSwap_23, K_strike, ...
+    discountCurve_23, pseudoCurve_23, vol_data_23, HazardRates(2), RecoveryRate, past_fixing_rate);
 
 % Plot the Expected Exposure profiles for the unwinding date
-plot_expected_exposures(scheduleSwap31.payDates, EE_300, EE_500, CDS_spreads);
+plot_expected_exposures(scheduleSwap_23.payDates, EE_300, EE_500, CDS_spreads);
 
 % Compute the final unwinding NPV adjusted for Counterparty Credit Risk (CVA)
 NPV_with_CVA_300 = NPV_RF - CVA_300;
@@ -104,26 +108,26 @@ diag_tenors = [15; 12; 10; 7; 5; 3; 1];
 gammas = [0; 0.5; 1];
 
 % Calibrate MHW parameters (with constant parameters and piecewise constant gamma) 
-[results_const, results_pwc, mkt_prices] = calibrate_multicurve_swaption_model(settlement, discountCurve, pseudoCurve, vol_data, diag_expiries, diag_tenors, gammas);
+[results_const, results_pwc, mkt_prices] = calibrate_multicurve_swaption_model(settlement_22, discountCurve_22, pseudoCurve_22, vol_data_22, diag_expiries, diag_tenors, gammas);
 
 %% Point 6: Hull-White Tree Pricing, Convergence, and Error Analysis
 
 % Calibrated Hull-White parameters (from Point 5)
-a_param     = 0.02;       % Mean reversion speed
-sigma_param = 1e-8;      % Volatility of the short rate
+a_param     = results_const(1).a;       % Mean reversion speed
+sigma_param = results_const(1).sigma;      % Volatility of the short rate
 
 % Discretization levels (Time steps per year)
 precision_levels = [1, 4, 12, 52, 365]; 
 
 % Pricing (CDS 300 bps)
 result_CDS_300 = run_hw_pricing_amortizing_swap_CVA(a_param, sigma_param, K_strike, ...
-    settlement, maturity_date_not_adjusted, precision_levels, notional_amortized, ...
-    RecoveryRate, HazardRates(1), discountCurve, pseudoCurve);
+    settlement_22, maturity_date_not_adjusted, precision_levels, notional_amortized, ...
+    RecoveryRate, HazardRates(1), discountCurve_22, pseudoCurve_22);
 
 % Pricing (CDS 500 bps)
 result_CDS_500 = run_hw_pricing_amortizing_swap_CVA(a_param, sigma_param, K_strike, ...
-    settlement, maturity_date_not_adjusted, precision_levels, notional_amortized, ...
-    RecoveryRate, HazardRates(2), discountCurve, pseudoCurve);
+    settlement_22, maturity_date_not_adjusted, precision_levels, notional_amortized, ...
+    RecoveryRate, HazardRates(2), discountCurve_22, pseudoCurve_22);
 
 % Convert results into MATLAB tables and print
 table_CDS_300 = struct2table(result_CDS_300);
